ce56feeda33647520e07462a8949207242fcfcc7
samrtisong <44254768+samrtisong@users.noreply.github.com>
2019-01-07 20:32:39 +0800

    Update jianzhi_offer.py

--- jianzhi_offer.py
+++ jianzhi_offer.py
@@ -4,3 +4,27 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+(2) # 求1+2+3+4+5+...+n 
+
+class Solution:
+    def Sum_Solution(self, n):
+        # write code here
+        # reduce 函数
+        return reduce(lambda item1, item2: item1+item2, range(n+1))
+        # 递归
+        if n==1:
+            return 1
+        return self.Sum_Solution(n-1)+n
+
+
