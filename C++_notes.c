5205cf94f5f50e39793bdaba65785d9866676b7c
samrtisong <44254768+samrtisong@users.noreply.github.com>
2019-01-10 20:08:27 +0800

    Update C++_notes.c

--- C++_notes.c
+++ C++_notes.c
@@ -10,7 +10,7 @@ int main()
    for(int i=0;i<str.length();i++){
 	   //cout << str.find(str[i]) << ',';
 	   if (ss.find(str[i]) == string::npos){         // 字符串中查找字符，有返回index,无返回一个很大的数：string::npos
-		   ss+=str[i];
+		   ss+=str[i];            // 字符串的连接 ‘+’
        }
    }
 	cout << ss << endl;
